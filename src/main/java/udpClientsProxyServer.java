b830908a71551c63fb47e489e6b228d0ab4f5836
mostafa <mostafa.ahmadi1375@yahoo.com>
2018-07-16 20:49:00 +0430

    it seems that GET coding is finished. but the problem of unknown characters has not been solved yet

--- src/main/java/udpClientsProxyServer.java
+++ src/main/java/udpClientsProxyServer.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -10,6 +9,7 @@ import java.util.ArrayList;
  */
 public class udpClientsProxyServer extends ProxyServer {
     ArrayList<udpClientsProxyServerThread> workers;
+
     public udpClientsProxyServer(String sourceIP, int sourcePort) {
         super(sourceIP, sourcePort);
         workers = new ArrayList<udpClientsProxyServerThread>();
@@ -21,24 +21,32 @@ public class udpClientsProxyServer extends ProxyServer {
         try {
             server = new DatagramSocket(sourcePort);
 
-        byte[] receiveData = new byte[1024];
-        byte[] sendData = new byte[1024];
-        System.out.println("Server listening on port " + sourcePort + " for udp!");
-        while (true) {
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            server.receive(receivePacket);
-            //inja stuck mishe
-            String sentence = new String(receivePacket.getData());
-            System.out.println("RECEIVED: " + sentence);
-            InetAddress IPAddress = receivePacket.getAddress();
-            int port = receivePacket.getPort();
-
-            try {
-            } catch (IOException e) {
-                e.printStackTrace();
+            byte[] receiveData = new byte[1024];
+            System.out.println("Server listening on port " + sourcePort + " for udp!");
+            while (true) {
+                boolean flag = false;
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                server.receive(receivePacket);
+                //inja stuck mishe
+                String sentence = new String(receivePacket.getData());
+                System.out.println("RECEIVED: " + sentence);
+                InetAddress IPAddress = receivePacket.getAddress();
+                int port = receivePacket.getPort();
+                for (int i = 0; i < workers.size(); i++) {
+                    if ((workers.get(i).getPort() == port)&&(workers.get(i).getIp().getAddress() == IPAddress.getAddress())){
+                        workers.get(i).ackReceived(sentence);
+                        flag = true;
+                        System.out.println("This was Ack!");
+                    }
+                }
+                if (!flag) {
+                    System.out.println("This was request!");
+                    udpClientsProxyServerThread jadid = new udpClientsProxyServerThread(IPAddress, port);
+                    jadid.setRequest(sentence);
+                    workers.add(jadid);
+                    jadid.start();
+                }
             }
-            new tcpClientsProxyServerThread(socket).start();
-        }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/udpClientsProxyServerThread.java
+++ src/main/java/udpClientsProxyServerThread.java
@@ -1,9 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.InetAddress;
-import java.net.URL;
+import java.net.*;
+import java.util.ArrayList;
 
 /**
  * Created by ahmadi on 7/16/18.
@@ -14,49 +13,94 @@ public class udpClientsProxyServerThread extends Thread {
     private String lastAck;
     private int lastIndexSent;
     private String URL;
-    private String request;
-    public udpClientsProxyServerThread( InetAddress ip, int port) {
+    private ArrayList<String> messages;
+
+    public udpClientsProxyServerThread(InetAddress ip, int port) {
         super();
         System.out.println("New thread starting!");
         this.port = port;
         this.ip = ip;
         lastIndexSent = 0;
+        messages = new ArrayList<String>();
     }
 
-    public void ackReceived(String ack){
+    public void ackReceived(String ack) throws IOException {
         lastAck = ack;
         sendNext();
     }
-    public void sendNext(){
+
+    public void sendNext() throws IOException {
+        if (Integer.parseInt(lastAck) == (2 + lastIndexSent) % 2)
+            return;
+        DatagramSocket clientSocket = new DatagramSocket();
+        lastIndexSent++;
+        String toBeSent = messages.get(lastIndexSent);
+        byte[] sendData;
+        sendData = toBeSent.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ip, port);
+        clientSocket.send(sendPacket);
+        clientSocket.close();
+    }
+
+    public InetAddress getIp() {
+        return ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setRequest(String request) {
+        request = request.toLowerCase();
+        char[] rchars = request.toCharArray();
+        int index = request.indexOf("host:") + 6;
+        //working with string
+        URL = "";
+        for (int i = index; i < request.length(); i++) {
+            if (rchars[i] == ' ')
+                break;
+            URL = URL + rchars[i];
+        }
+        System.out.println("URL: " + URL);
 
     }
-    public void setURL(String URL){
-        this.URL = URL;
-    }
-    public void setRequest(String request){
-        this.request = request;
-    }
+
     @Override
     public void run() {
         System.out.println("Running thread");
-        String clientEntry = request;
         try {
-            // address ro darbiar, resolve ro seda bezan, segmant kon, sendNext ro seda kon
-        }
-        catch (Exception e){
+            // get request to web-server
+            String response = resolve(URL);
+            // segmenting data
+            for (int i = 0; i <= (int) (response.length()) / 1000; i++) {
+                String edame = "1";
+                String seqNum = "0";
+                int finish = (i + 1) * 1000 - 1;
+                if (i == (int) (response.length()) / 1000) {
+                    edame = "0";
+                    finish = response.length() - 1;
+                }
+                if (i % 2 == 1) seqNum = "1";
+                messages.add(edame + seqNum + response.substring(i * 1000, finish));
+            }
+            //sending first part
+            lastIndexSent = -1;
+            sendNext();
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
         System.out.println("Thread finishing!");
     }
 
     //works fine
+    //sends get request and returns answer
     private String resolve(String GET_URL) throws IOException {
         URL obj = new URL(GET_URL);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
         int responseCode = con.getResponseCode();
         if (responseCode == HttpURLConnection.HTTP_OK) {
-            // success
             BufferedReader in = new BufferedReader(new InputStreamReader(
                     con.getInputStream()));
             String inputLine;
